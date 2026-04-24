b68bab682d16dd8ac6ec3508721b79d0aff3bc7f
Paul Carpenter <161557030+pauly7610@users.noreply.github.com>
2026-04-24 00:38:47 -0400

    feat: load workspace context styles

--- generators/web/src/main.tsx
+++ generators/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppShell from './AppShell'
 import './console.css'
+import './consoleWorkspace.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
