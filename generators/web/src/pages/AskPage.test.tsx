c2ab8f20cc50d238d59d61f99870aea9a7ad9a1d
Paul Carpenter <161557030+pauly7610@users.noreply.github.com>
2026-04-24 01:06:14 -0400

    test: remove unused ask page render import

--- generators/web/src/pages/AskPage.test.tsx
+++ generators/web/src/pages/AskPage.test.tsx
@@ -1,5 +1,5 @@
 // @vitest-environment jsdom
-import { cleanup, render, screen } from '@testing-library/react'
+import { cleanup, screen } from '@testing-library/react'
 import { afterEach, describe, expect, it } from 'vitest'
 
 import { consoleDevelopmentSlice } from '../content/consoleDevelopmentSlice'
